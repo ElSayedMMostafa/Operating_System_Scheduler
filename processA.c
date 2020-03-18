2710957e3831e5b3ae62d7c8f436aa3d38e9214e
ElSayedMMostafa <45875353+ElSayedMMostafa@users.noreply.github.com>
2020-03-18 19:08:15 +0200

    Update processA.c

--- processA.c
+++ processA.c
@@ -11,14 +11,14 @@
 int main(int agrc, char * argv[])
 {
 int c = atoi(argv[1]); 
-printf("Hello I'm Process A &my processing time = %d \n",c);
+printf("Hello I'm Process A &my processing time = %d & PID=%d \n",c,getpid());
 
 //HERE WHILE LOOP UNTIL THE TIME ENDS
  sleep(c); //replace 1 with c
 //HERE WHILE LOOP UNTIL THE TIME ENDS
 printf("I am FINISHED \n");
 kill(getpgrp(),SIGCONT);
-//exit(2); //Sent to the scheduler
+exit(2); //Sent to the scheduler
 }
 
 
