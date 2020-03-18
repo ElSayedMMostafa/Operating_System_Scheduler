c7f68009742c2ac7edc1bfc3f66a5d5e1e11770f
ElSayedMMostafa <45875353+ElSayedMMostafa@users.noreply.github.com>
2020-03-18 14:21:30 +0200

    Update processA.c

--- processA.c
+++ processA.c
@@ -6,20 +6,20 @@
 #include <sys/wait.h>
 #define STDIN_FILENO 0
 #define STDOUT_FILENO 1
+
+
 int main(int agrc, char * argv[])
 {
-printf("Hello I'm Process A & I'm running\n");
-pid_t parent_pid = atoi(argv[1]); //convert to integer
-int c = atoi(argv[2]); 
-printf("my processing time = %d \n",c);
-printf("my ppid            = %d \n",parent_pid);
+int c = atoi(argv[1]); 
+printf("Hello I'm Process A &my processing time = %d \n",c);
 
 //HERE WHILE LOOP UNTIL THE TIME ENDS
- sleep(1); //replace 1 with c
+ sleep(c); //replace 1 with c
 //HERE WHILE LOOP UNTIL THE TIME ENDS
 printf("I am FINISHED \n");
- //exit(4); //Is not sent or recieved
-  kill(parent_pid,SIGINT);
+kill(getpgrp(),SIGCONT);
+//exit(2); //Sent to the scheduler
 }
+
 
 
