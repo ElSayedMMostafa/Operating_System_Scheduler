357466f64eaa2d217f867cbdbfe5bfb00f3a7b29
ElSayedMMostafa <45875353+ElSayedMMostafa@users.noreply.github.com>
2020-03-27 11:02:59 +0200

    Update pg_trial.c

--- pg_trial.c
+++ pg_trial.c
@@ -23,5 +23,18 @@ int main(int argc, char * argv[]){
  struct msgbuff pg_message; //Instance of the buffer (Sent by the process generator)
  msgqid = msgget(msg_key,IPC_CREAT|0644);
  if(msgqid == -1) perror("Error in intializing");
+ 
+//The message
+  struct Process_data my_Data = {'S', 1, 7, 10, 3};
+   // OR like
+ /* struct Process_data my_Data;
+  my_Data.name='R';my_Data.processing_time=12;
+  my_Data.arrival_time=2; my_Data.starting_time=3;
+  my_Data.priority=1; */
+
+  pg_message.mtype=getpid(); //Some sychronization..
+  pg_message.data = my_Data;
+ 
+  int sending_status = msgsnd(msgqid,&pg_message,sizeof(pg_message.data),!IPC_NOWAIT);
  while(1);
 }
