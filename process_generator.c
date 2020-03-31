2d5833ca5a44f8ffd8a77b90b7107db2574c6427
ElSayedMMostafa <45875353+ElSayedMMostafa@users.noreply.github.com>
2020-03-31 23:13:17 +0200

    Update process_generator.c

--- process_generator.c
+++ process_generator.c
@@ -1,27 +1,86 @@
-#include "headers.h"
+#include <stdio.h>
+#include <stdlib.h>
+ 
+/*
+struct Process_data 
+{ 
+   char name; 
+   int priority; 
+   int processing_time;  
+   int starting_time;
+   int arrival_time;
+   int remaining_time;
+};
+*/
+struct Process_data 
+{ 
+   int name;
+   int arrival_time;
+   int processing_time; 
+   int remaining_time; 
+   int priority;
+   int waiting_time;
+   int process_pid;
+};
 
-void clearResources(int);
+typedef struct {
+    int priority;
+    struct Process_data data;
+} node_t;
 
-int main(int argc, char * argv[])
-{
-    signal(SIGINT, clearResources);
-    // TODO Initialization
-    // 1. Read the input files.
-    // 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any.
-    // 3. Initiate and create the scheduler and clock processes.
-    // 4. Use this function after creating the clock process to initialize clock
-    initClk();
-    // To get time use this
-    int x = getClk();
-    printf("current time is %d\n", x);
-    // TODO Generation Main Loop
-    // 5. Create a data structure for processes and provide it with its parameters.
-    // 6. Send the information to the scheduler at the appropriate time.
-    // 7. Clear clock resources
-    destroyClk(true);
+typedef struct {
+    node_t *nodes;
+    int len;
+    int size;
+} heap_t;
+ 
+void push (heap_t *h, int priority, struct Process_data data) {
+    if (h->len + 1 >= h->size) {
+        h->size = h->size ? h->size * 2 : 4;
+        h->nodes = (node_t *)realloc(h->nodes, h->size * sizeof (node_t));
+    }
+    int i = h->len + 1;
+    int j = i / 2;
+    while (i > 1 && h->nodes[j].priority > priority) {
+        h->nodes[i] = h->nodes[j];
+        i = j;
+        j = j / 2;
+    }
+    h->nodes[i].priority = priority;
+    h->nodes[i].data = data;
+    h->len++;
+}
+ 
+struct Process_data pop (heap_t *h) {
+    int i, j, k;
+    if (!h->len) {
+     struct Process_data NULL_Process_data = {-1,-1,-1,-1}; //the null value
+        return NULL_Process_data  ; 
+    }
+    struct Process_data data = h->nodes[1].data;
+ 
+    h->nodes[1] = h->nodes[h->len];
+ 
+    h->len--;
+ 
+    i = 1;
+    while (i!=h->len+1) {
+        k = h->len+1;
+        j = 2 * i;
+        if (j <= h->len && h->nodes[j].priority < h->nodes[k].priority) {
+            k = j;
+        }
+        if (j + 1 <= h->len && h->nodes[j + 1].priority < h->nodes[k].priority) {
+            k = j + 1;
+        }
+        h->nodes[i] = h->nodes[k];
+        i = k;
+    }
+    return data;
 }
 
-void clearResources(int signum)
-{
-    //TODO Clears all resources in case of interruption
+// The next function is mine..
+int peak_time (heap_t *h){
+   if(h->len != 0) {return h->nodes[1].data.arrival_time;}
+   return -1;  
 }
