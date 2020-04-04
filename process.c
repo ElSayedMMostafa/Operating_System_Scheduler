34bbdb42478124ae76be1520ab0d14898bfc3aa5
ElSayedMMostafa <45875353+ElSayedMMostafa@users.noreply.github.com>
2020-04-04 03:24:45 +0200

    Update process.c

--- process.c
+++ process.c
@@ -14,7 +14,7 @@ int main(int agrc, char * argv[])
 
 clock_t past_clk=0, current_clk=0;
 double seconds_elapsed1=0,seconds_elapsed2=0;
-//int c = atoi(argv[1]);  ----> I use it temporarily with SRTN till we concatenate the whole project
+int c = atoi(argv[1]); 
 past_clk=clock();
     seconds_elapsed1=(double)past_clk/CLOCKS_PER_SEC;
     
@@ -25,7 +25,7 @@ past_clk=clock();
         seconds_elapsed2=(double)current_clk/CLOCKS_PER_SEC;
     }
 kill(getppid(),SIGUSR1); //JUST FOR RR-->Doesnot affect HPF --> But should be off in case of SRTN
-exit(20);
+//exit(20);
 }
 
 
